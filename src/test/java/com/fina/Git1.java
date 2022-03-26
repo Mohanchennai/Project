24d9f7c7abdd6ecddf03d81e0c212848f9700643
mohanchennai <msundaram285@gmail.com>
2022-03-26 16:05:17 +0530

    sucess

---
+++ src/test/java/com/fina/Git1.java
@@ -0,0 +1,28 @@
+package com.fina;
+
+public class Git1 {
+public static void main(String[] args) {
+	
+	int a=120,b=230;
+	System.out.println("before swapping");
+	
+	System.out.println("value of a"+a);
+	System.out.println("value of b"+b);
+	
+	a=a+b;//120+230
+	b=a-b;//350-230
+	a=a-b;//350-120
+	
+	System.out.println("after swapping");
+	
+	System.out.println("value of a"+a);
+	System.out.println("value of b"+b);
+	
+	
+	
+	
+	
+	
+	
+}
+}
